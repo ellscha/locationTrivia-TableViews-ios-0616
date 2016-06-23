c58af5595abaaaba5ef03eefb4aa21f433972e0d
ellscha <escharlin@wesleyan.edu>
2016-06-23 10:57:36 -0400

    done.

---
+++ locationTrivia-tableviews/FISLocationsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISLocationsTableViewController.h
+//  locationTrivia-tableviews
+//
+//  Created by Elli Scharlin on 6/23/16.
+//  Copyright © 2016 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISLocationsTableViewController : UITableViewController
+@property (strong, nonatomic) NSArray *locations;
+@end
---
+++ locationTrivia-tableviews/FISTriviaTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  FISTriviaTableViewController.h
+//  locationTrivia-tableviews
+//
+//  Created by Elli Scharlin on 6/23/16.
+//  Copyright © 2016 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISLocation.h"
+@interface FISTriviaTableViewController : UITableViewController
+@property (strong, nonatomic) NSArray *trivia;
+
+@end
